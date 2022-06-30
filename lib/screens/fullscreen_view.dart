c7a7265b0da1c731c8327abe379fbf62767705c4
Harish Sundaresh <yesharish@yahoo.in>
2022-06-30 22:43:40 +1000

    Page navigation and exit button for Fullscreen View

--- lib/screens/fullscreen_view.dart
+++ lib/screens/fullscreen_view.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_acrylic/flutter_acrylic.dart';
+import 'package:lastpage/models/lastpage_colors.dart';
 import 'package:lastpage/models/user_uploads/user_upload_info.dart';
 import 'package:photo_view/photo_view.dart';
 
@@ -18,6 +19,13 @@ class _FullscreenViewState extends State<FullscreenView> {
   void initState() {
     _toggleFullScreen(true);
     super.initState();
+    Future.delayed(const Duration(seconds: 5), () {
+      if (mounted) {
+        setState(() {
+          _visible = false;
+        });
+      }
+    });
   }
 
   @override
@@ -34,18 +42,116 @@ class _FullscreenViewState extends State<FullscreenView> {
     }
   }
 
+  var previewIdx = 0;
+  var _visible = false;
+
   @override
   Widget build(BuildContext context) {
     UserUploadInfo uploadInfo =
         ModalRoute.of(context)!.settings.arguments as UserUploadInfo;
     List<String> localFilePaths = uploadInfo.localFilePaths!;
-    var idx = 0;
-    return Container(
-      child: PhotoView(
-        imageProvider: FileImage(
-          File(localFilePaths[idx]),
+    var maxIdx = localFilePaths.length - 1;
+    return MouseRegion(
+      onHover: (event) {
+        if (mounted && !_visible) {
+          setState(() {
+            _visible = true;
+          });
+        }
+        Future.delayed(const Duration(seconds: 5), (() {
+          if (mounted && _visible) {
+            setState(() {
+              _visible = false;
+            });
+          }
+        }));
+      },
+      child: Stack(children: [
+        Container(
+          child: PhotoView(
+            imageProvider: FileImage(
+              File(localFilePaths[previewIdx]),
+            ),
+          ),
         ),
-      ),
+        Positioned(
+          top: 50,
+          right: 25,
+          child: Visibility(
+            visible: _visible,
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  // width: 1.0,
+                  color: LastpageColors.lightGrey,
+                ),
+                borderRadius: BorderRadius.circular(5.0),
+                color: LastpageColors.white,
+              ),
+              child: Center(
+                child: Column(children: [
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      _toggleFullScreen(false);
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(Icons.close),
+                    label: const Text("CLOSE"),
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.red),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        OutlinedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                LastpageColors.lightGrey),
+                          ),
+                          onPressed: previewIdx > 0
+                              ? () {
+                                  if (previewIdx > 0) {
+                                    setState(() {
+                                      previewIdx -= 1;
+                                    });
+                                  }
+                                }
+                              : null,
+                          child: const Icon(Icons.arrow_left),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        OutlinedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                LastpageColors.lightGrey),
+                          ),
+                          onPressed: previewIdx < maxIdx
+                              ? () {
+                                  if (previewIdx < maxIdx) {
+                                    setState(() {
+                                      previewIdx += 1;
+                                    });
+                                  }
+                                }
+                              : null,
+                          child: const Icon(Icons.arrow_right),
+                        ),
+                      ]),
+                ]),
+              ),
+            ),
+          ),
+        )
+      ]),
     );
   }
 }
